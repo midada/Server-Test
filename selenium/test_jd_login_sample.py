7095114749fbda143590ee993d4c66cffac2e1c5
midada <sx.work@outlook.com>
2016-06-28 11:37:54 +0800

    selenium sample

--- selenium/test_jd_login_sample.py
+++ selenium/test_jd_login_sample.py
@@ -49,6 +49,7 @@ def login(driver,check_element,username,password):
         text = driver.find_element_by_xpath(check_element).text       
         assert text is not None
     except:
+        text = driver.find_element_by_xpath(check_element).text 
         print(u" -> Test_Input: {0},{1} \n\tTest_Run,return: {2} \n\tTest_Results_judge: 不符合预期结果,测试失败." \
                     .format(username,password,text))
     else:
